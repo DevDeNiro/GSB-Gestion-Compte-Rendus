ded3555026645e672c589c924aaee012f3c02ff2
Erwan8989 <77726753+Erwan8989@users.noreply.github.com>
2022-02-23 09:25:24 +0100

    Séparation des 3 pages

--- app/src/main/java/com/example/gsb_doctors/ui/main/PageViewModel.java
+++ app/src/main/java/com/example/gsb_doctors/ui/main/PageViewModel.java
@@ -12,7 +12,15 @@ public class PageViewModel extends ViewModel {
     private LiveData<String> mText = Transformations.map(mIndex, new Function<Integer, String>() {
         @Override
         public String apply(Integer input) {
-            return "Hello world from section: " + input;
+            if (input == 1) { //Page saisir
+                return "Hello world from section: " + input;
+            }
+            else if (input == 2) { //Page accueil
+                return "2 " + input;
+            }
+            else { //Page consulter
+                return "Fin " + input;
+            }
         }
     });
 
