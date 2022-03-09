a31b047cb57cf6c7cb5c96a29ed312a29a0af64b
Erwan8989 <77726753+Erwan8989@users.noreply.github.com>
2022-03-09 07:44:50 +0100

    Disposition des données dans détail (en cours)

--- app/src/main/java/com/example/gsb_doctors/detail.java
+++ app/src/main/java/com/example/gsb_doctors/detail.java
@@ -26,16 +26,18 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.time.Year;
 
 public class detail extends AppCompatActivity {
 
-    ListView listView;
+    TextView titre, age;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detail);
 
-        listView = (ListView) findViewById(R.id.listView2);
+        titre = (TextView) findViewById(R.id.titre);
+        age = (TextView) findViewById(R.id.age);
 
         Intent intent = getIntent();
         String text = intent.getStringExtra("id1");
@@ -92,13 +94,17 @@ public class detail extends AppCompatActivity {
 
     private void loadIntoListView(String json) throws JSONException {
         JSONArray jsonArray = new JSONArray(json);
-        String[] heroes = new String[jsonArray.length()];
-        for (int i = 0; i < jsonArray.length(); i++) {
-            JSONObject obj = jsonArray.getJSONObject(i);
-            heroes[i] = obj.getString("prenom") + " " + obj.getString("nom") + System.getProperty("line.separator") + "Date : " + obj.getString("rdv");
-        }
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, heroes);
-        listView.setAdapter(arrayAdapter);
+        String getNomPrenom, getAge;
+        int i = 0;
+        int year = Year.now().getValue();
+        JSONObject obj = jsonArray.getJSONObject(i);
+        getNomPrenom = obj.getString("prenom") + " " + obj.getString("nom");
+        getAge = obj.getString("rdv");
+
+        //getAge = year - 5;
+        
+        titre.setText(getNomPrenom);
+        age.setText(getAge);
 
     }
 
